142c16e4b3ab370f97015dc312c3841d471f1d9f
ikervazquezlopez <ikervazquezlopez@u.boisestate.edu>
2021-02-23 11:20:44 -0700

    Corrected forward transform on dct-on-triangles-matlab.py, now it is computing it correctly but the boundary diagonal is getting some weird values for a triangle full of ones.

--- dct-on-triangles-matlab.py
+++ dct-on-triangles-matlab.py
@@ -4,6 +4,29 @@ import math
 from scipy import io, integrate, linalg, signal
 from scipy.sparse.linalg import eigs
 
+
+def generate_base_img(Tkl_array,triangle_h):
+    triangle = np.zeros((triangle_h,triangle_h))
+    i=0
+    for y in range(0,triangle_h):
+        for x in range(0,y+1):
+            triangle[y,x] = Tkl_array[i]
+            i = i+1
+    return triangle
+
+def generate_basis_img(dtt, triangle_h):
+    i = 0
+    basis = np.zeros((triangle_h*triangle_h, triangle_h*triangle_h))
+    for l in range(0,triangle_h):
+        for k in range(0,l+1):
+            y_start = l*triangle_h
+            y_end = y_start + triangle_h
+            x_start = k*triangle_h
+            x_end = x_start + triangle_h
+            basis[y_start:y_end,x_start:x_end] = generate_base_img(dtt[i],triangle_h)
+            i = i+1
+    return basis
+
 def evalChebyshevB2(idxK,idxL,arg1,arg2):
     value = 0.25* (np.cos(2*math.pi*(idxK * arg1 + idxL * arg2)) +
                 np.cos(2*math.pi*((idxK + idxL) * arg1 - idxL * arg2)) +
@@ -38,15 +61,22 @@ def createTriangleDCT(sizeN):
     triangleDCT = []
     for i in range(0,chebIdx.shape[1]):
         triangleDCT.append(evalChebyshevB2(chebIdx[0,i],chebIdx[1,i],zerosOfIdeal[0,:],zerosOfIdeal[1,:]))
-    triangleDCT = np.round(np.array(triangleDCT),decimals=4).T
-
+    triangleDCT = np.round(np.array(triangleDCT),decimals=4)
+    print(triangleDCT.shape)
     return triangleDCT
 
 
 if __name__ == '__main__':
-    sizeN = 8
+    sizeN = 16
 
     dtt = createTriangleDCT(sizeN);
+    print(dtt[0])
+
+    b = generate_basis_img(dtt, sizeN)
+    b = cv2.resize(b, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
+    cv2.imshow("basis", b)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     J = np.ones((sizeN, sizeN))
     J = np.tril(J)
@@ -66,6 +96,7 @@ if __name__ == '__main__':
     tmp = np.uint8(linalg.lstsq(dtt, triangleTransformedPart))
     print(J)
     """
+
     # Convert to image to visually check the image energy in the frequency domain
     i=0
     triangleDCT = np.empty((sizeN,sizeN))
